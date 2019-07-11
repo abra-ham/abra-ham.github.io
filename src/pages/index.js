97e6c620a900104b3ec64c1bb8766db426e67d90
Antonio Abraham <lopezramirez.antonioabraham@gmail.com>
2019-07-10 20:51:52 -0500

    Initialize repo, add main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import myPicture from '../images/me.jpg';
+
+import * as styles from './index.module.scss'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Abraham López | Software Engineer" />
+    <article className={styles.index}>
+      <header className={styles.header}>
+        <figure>
+          <img src={myPicture} alt="Me at Corona Capital"/>
+        </figure>
+        <h1>Abraham López</h1>
+        <h2>Software Engineer</h2>
+        <div>
+          <a href="https://www.linkedin.com/in/abraham-l%C3%B3pez-76b993138/">LinkedIn</a>
+          <a href="https://github.com/abra-ham">GitHub</a>
+        </div>
+      </header>
+      <section className={styles.aboutMe}>
+        <p>
+          A software enginer born and raised in Guadalajara, Jalisco.
+          Abraham likes to spend his days coding, hanging out with his girlfriend and
+          being a helping hand to his single mother.
+        </p>
+        <p>
+          Abraham really enjoys:
+        </p>
+        <ul>
+          <li>Crafting tasteful front end experiences</li>
+          <li>Writing software that scales</li>
+          <li>Implementing all tech he reads about on Medium (sarcasm?)</li>
+          <li>Learning about principles, patterns and paradigms</li>
+          <li>Writing unit and integration testing</li>
+          <li>In case his CTO is reading this, he always uses a TDD approach ;)</li>
+          <li>Writing in third person!</li>
+        </ul>
+      </section>
+    </article>
   </Layout>
 )
 
